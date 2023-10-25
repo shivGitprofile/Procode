130208fd5e2970a40a72ce36cf49c9d781f94064
shivGitprofile <121003675+shivGitprofile@users.noreply.github.com>
2023-10-25 19:40:38 +0530

    Add files via upload

---
+++ api.js
@@ -0,0 +1,106 @@
+const express = require("express")
+const app = express()
+const bodyP = require("body-parser")
+const compiler = require("compilex")
+const options = { stats: true }
+const cors = require("cors"); 
+compiler.init(options)
+// Import the cors middleware
+app.use(cors());
+app.use(bodyP.json())
+app.use("/codemirror-5.65.15", express.static("C:/Users/kumar/OneDrive/Desktop/PROCODE2.0/codemirror-5.65.15"))
+app.get("/", function (req, res) {
+   // compiler.flush(function () {
+     //   console.log("deleted")
+    //  })
+    res.sendFile("C:/Users/kumar/OneDrive/Desktop/PROCODE2.0/index.html")
+})
+app.post("/compile",async function (req, res) {
+    var code = req.body.code
+    var input = req.body.input
+    var lang = req.body.lang
+    try {
+
+        if (lang == "Cpp") {
+            if (!input) {
+                var envData = { OS: "windows", cmd: "g++", options: { timeout: 10000 } }; // (uses g++ command to compile )
+                compiler.compileCPP(envData, code, function (data) {
+                    if (data.output) {
+                        res.send(data);
+                    }
+                    else {
+                        res.send({ output: "error" })
+                    }
+                });
+            }
+            else {
+                var envData = { OS: "windows", cmd: "g++", options: { timeout: 10000 } }; // (uses g++ command to compile )
+                compiler.compileCPPWithInput(envData, code, input, function (data) {
+                    if (data.output) {
+                        res.send(data);
+                    }
+                    else {
+                        res.send({ output: "error" })
+                    }
+                });
+            }
+        }
+        else if (lang == "Java") {
+            if (!input) {
+                var envData = { OS: "windows" };
+                compiler.compileJava(envData, code, function (data) {
+                    if (data.output) {
+                        res.send(data);
+                    }
+                    else {
+                        res.send({ output: "error" })
+                    }
+                })
+            }
+            else {
+                //if windows  
+                var envData = { OS: "windows" };
+                //else
+                compiler.compileJavaWithInput(envData, code, input, function (data) {
+                    if (data.output) {
+                        res.send(data);
+                    }
+                    else {
+                        res.send({ output: "error" })
+                    }
+                })
+            }
+        }
+        else if (lang == "Python") {
+            if (!input) {
+                var envData = { OS: "windows",options: { timeout: 10000 } };
+                compiler.compilePython(envData, code,async function (data) {
+                    if (data.output) {
+                     //   var d = await oData.json();
+        //output.value = d.output;
+                      await res.send(data);
+                    }
+                    else {
+                        res.send({ output: "error" })
+                    }
+                });
+            }
+            else {
+                var envData = { OS: "windows" };
+                compiler.compilePythonWithInput(envData, code, input, function (data) {
+                    if (data.output) {
+                        res.send(data);
+                    }
+                    else {
+                        res.send({ output: "error" })
+                    }
+                });
+            }
+        }
+    }
+    catch (e) {
+        console.log("error")
+    }
+})
+
+app.listen(8000)
